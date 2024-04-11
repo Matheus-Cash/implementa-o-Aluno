5fa94700e911c31dd68fbd5cf51621dad1d64095
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2024-04-11 17:00:21 -0300

    4

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const dataatual=Date();
 let dataniver=prompt (''Data do próximo Niver)
 data=new Date (dataniver + ''T23:59:59);
-let diasquefaltam = Math.floor((dataniver-dataatual)/86400000),
+let diasquefaltam = Math.floor((dataniver-dataatual)/86400000);
 documento.queryselector(''#dias_restantes").textcontet=diasquefaltam
 
